3854ae562bdc054501e0925870f0ac7ae8e4dfc4
Mark Rickerby <me@maetl.net>
2015-11-07 14:40:03 +1100

    Iterate through cardinal directions instead of manually specifiying each
    Also run formatter over the code to clean up spacing and indentation.

--- lib/src/content/desert.dart
+++ lib/src/content/desert.dart
@@ -1,7 +1,5 @@
 library hauberk.content.desert;
 
-import 'dart:math' as math;
-
 import 'package:piecemeal/piecemeal.dart';
 
 import '../engine.dart';
@@ -9,10 +7,9 @@ import 'stage_builder.dart';
 import 'tiles.dart';
 
 class Desert extends StageBuilder {
-
   /// Number of iterations of the cell transformation rules to run on the grid.
   ///
-  /// Less iterations create more spiky terrain, with a greater likelihood of
+  /// Fewer iterations leads to more spiky terrain, with a greater likelihood of
   /// unreachable spaces being present.
   int get generatorIterations => 4;
 
@@ -35,8 +32,8 @@ class Desert extends StageBuilder {
     fill(Tiles.canyon);
 
     // Randomly place walls inside the inner grid.
-    for (var y = 1; y < stage.height-1; y++) {
-      for (var x = 1; x < stage.width-1; x++) {
+    for (var y = 1; y < stage.height - 1; y++) {
+      for (var x = 1; x < stage.width - 1; x++) {
         if (rng.range(100) > initialWallPercent) {
           setTile(new Vec(x, y), Tiles.sand);
         }
@@ -47,13 +44,15 @@ class Desert extends StageBuilder {
     // - Wall cells with less than two adjacent walls become floors.
     // - Floor cells with more than five adjacent walls become walls.
     for (var i = 0; i < generatorIterations; i++) {
-      for (var y = 1; y < stage.height-1; y++) {
-        for (var x = 1; x < stage.width-1; x++) {
+      for (var y = 1; y < stage.height - 1; y++) {
+        for (var x = 1; x < stage.width - 1; x++) {
           var cell = new Vec(x, y);
           var adjacentWalls = countAdjacentWalls(cell);
-          if (getTile(cell) == Tiles.canyon && adjacentWalls < wallToFloorUpperThreshold) {
+          if (getTile(cell) == Tiles.canyon &&
+              adjacentWalls < wallToFloorUpperThreshold) {
             setTile(cell, Tiles.sand);
-          } else if (getTile(cell) == Tiles.sand && adjacentWalls > floorToWallLowerThreshold) {
+          } else if (getTile(cell) == Tiles.sand &&
+              adjacentWalls > floorToWallLowerThreshold) {
             setTile(cell, Tiles.canyon);
           }
         }
@@ -61,15 +60,17 @@ class Desert extends StageBuilder {
     }
 
     // Remove walls with adjacent floor cells in each cardinal direction.
-    for (var y = 1; y < stage.height-1; y++) {
-      for (var x = 1; x < stage.width-1; x++) {
+    for (var y = 1; y < stage.height - 1; y++) {
+      for (var x = 1; x < stage.width - 1; x++) {
         var cell = new Vec(x, y);
 
         if (getTile(cell) == Tiles.sand) continue;
 
         // Check if this wall is blocking floor tiles on a North/South or East/West axis
-        if ((getTile(cell.offset(0, -1)) == Tiles.sand && getTile(cell.offset(0, 1)) == Tiles.sand) ||
-            (getTile(cell.offset(1, 0)) == Tiles.sand && getTile(cell.offset(-1, 0)) == Tiles.sand)) {
+        if ((getTile(cell.offset(0, -1)) == Tiles.sand &&
+                getTile(cell.offset(0, 1)) == Tiles.sand) ||
+            (getTile(cell.offset(1, 0)) == Tiles.sand &&
+                getTile(cell.offset(-1, 0)) == Tiles.sand)) {
           setTile(cell, Tiles.sand);
         }
       }
@@ -84,42 +85,16 @@ class Desert extends StageBuilder {
         setTile(pos, rng.item(walls));
       }
     }
-
   }
 
   // Count the number of adjacent tiles that are walls.
   int countAdjacentWalls(Vec pos) {
     var walls = 0;
 
-    if (getTile(pos.offset(-1, -1)) == Tiles.canyon) {
-      walls++;
-    }
-
-    if (getTile(pos.offset(0, -1)) == Tiles.canyon) {
-      walls++;
-    }
-
-    if (getTile(pos.offset(1, -1)) == Tiles.canyon) {
-      walls++;
-    }
-
-    if (getTile(pos.offset(1, 1)) == Tiles.canyon) {
-      walls++;
-    }
-
-    if (getTile(pos.offset(0, -1)) == Tiles.canyon) {
-      walls++;
-    }
-
-    if (getTile(pos.offset(-1, 1)) == Tiles.canyon) {
-      walls++;
-    }
-
-    if (getTile(pos.offset(-1, 0)) == Tiles.canyon) {
-      walls++;
+    for (var dir in Direction.ALL) {
+      if (getTile(pos.offset(dir.x, dir.y)) == Tiles.canyon) walls++;
     }
 
     return walls;
   }
-
 }
